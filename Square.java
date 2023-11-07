84094dec675a7f17fe32734661fa72cb6c0f4ba1
ndyky07 <sandipamungkas600@gmail.com>
2023-11-07 11:06:51 +0700

    commit

--- Square.java
+++ Square.java
@@ -9,7 +9,7 @@ public class Square {
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
 
-        for (int iOuter = 1; iOuter <= N; iOuter++) {
+        for (int iOuter = 0; iOuter <= N; iOuter++) {
             for (int i = 1; i <= N; i++) {
 
                 System.out.print("*");
