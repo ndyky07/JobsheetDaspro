4a789685a9bdffde64c3388295f1498c5e9217e7
ndyky07 <sandipamungkas600@gmail.com>
2023-11-07 12:21:13 +0700

    commit

---
+++ NestedLoop_NIM.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class NestedLoop_NIM {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int [][] temps = new int [5][7];
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = scanner.nextInt();
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.print("Kota ke-" + (i + 1) + ": ");
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print(temps[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
