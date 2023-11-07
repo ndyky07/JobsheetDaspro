74e447f1e92dc83bbcf232b14f4dd88637a038c7
ndyky07 <sandipamungkas600@gmail.com>
2023-11-07 08:14:29 +0700

    commit

---
+++ ArryBilangan14.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArryBilangan14{
+    public static void main(String[] args){
+
+    double bil[] = {5.0, 12867, 7.5, 2000000 };
+     bil [0] = 5.0;
+     bil [1] = 12867;
+     bil [2] = 7.5;
+     bil [3] = 2000000;
+
+        for (int i = 0; i <=4; i++){
+            System.out.println(bil[i]);
+    }
+ 
+
+}
+}
