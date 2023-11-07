1cbf94a34584de863f6a512de1d8e69ba43fdd24
ndyky07 <sandipamungkas600@gmail.com>
2023-11-07 15:04:05 +0700

    commit

--- Jobsheet 11/NestedLoop_NIM.java
+++ Jobsheet 11/NestedLoop_NIM.java
@@ -6,7 +6,7 @@ public class NestedLoop_NIM {
 
         Scanner scanner = new Scanner(System.in);
 
-        int [][] temps = new int [5][7];
+        int[][] temps = new int[5][7];
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + i);
@@ -16,15 +16,19 @@ public class NestedLoop_NIM {
             }
             System.out.println();
         }
-        
-        int kotaIndex = 1;
-        for (int [] kotaTemps : temps) {
-            System.out.print("Kota ke-" + kotaIndex + ": ");
-            for (int temp : kotaTemps) {
+
+        for (int i = 0; i < temps.length; i++) {
+            int kotaTotal = 0;
+            for (int j =0; j < temps[0].length; j++) {
+                kotaTotal += temps[i][j];
+            }
+            double kotaAverage = (double) kotaTotal / temps[0].length;
+
+            System.out.println("Kota ke-" + (i + 1) + ":  ");
+            for (int temp : temps [i]){
                 System.out.print(temp + " ");
             }
-            System.out.println();
-            kotaIndex++;
+            System.out.println("Rata-rata : " + kotaAverage);
         }
     }
 }
