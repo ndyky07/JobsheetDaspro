64cc0d522f95976fad64afbce956fb7b9516cd1c
ndyky07 <sandipamungkas600@gmail.com>
2023-11-21 09:43:54 +0700

    commit

---
+++ Jobsheet 13/Terimakasih_14.java
@@ -0,0 +1,10 @@
+public class Terimakasih_14 {
+    public static void UcapanTerimakasih(){
+        System.out.println("Thank you being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void main(String[] args){
+        UcapanTerimakasih();
+    }
+}
